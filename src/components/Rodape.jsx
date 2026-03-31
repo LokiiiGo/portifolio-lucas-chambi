989b0e8346b7583d209e9f9eca4ee6e44962e533
LokiiiGo <lucascalle880@gmail.com>
2026-03-31 19:21:15 -0300

    removendo a borda no componente Rodape

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,6 +1,6 @@
 function Rodape() {
   return (
-    <footer className="bg-transparent text-white text-center py-6 sm:py-8 px-4 border-2 border-white">
+    <footer className="bg-transparent text-white text-center py-6 sm:py-8 px-4">
       <p className="text-sm sm:text-base">
         {new Date().getFullYear()} Portifólio de Lucas Chambi. Todos os direitos reservados.
       </p>

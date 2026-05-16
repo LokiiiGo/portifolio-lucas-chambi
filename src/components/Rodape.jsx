d1a6ae445a699897a47913ccba85d88290035b9f
LokiiiGo <lucascalle880@gmail.com>
2026-05-16 00:42:15 -0300

    Fazendo mudança no rodapé

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -2,7 +2,7 @@ function Rodape() {
   return (
     <footer className="bg-transparent text-white text-center py-6 sm:py-8 px-4">
       <p className="text-sm sm:text-base">
-        {new Date().getFullYear()} Portifólio de Lucas Chambi. Todos os direitos reservados.
+        &copy; {new Date().getFullYear()} Portifólio de Lucas Chambi
       </p>
     </footer>
   )
